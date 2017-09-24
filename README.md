ad50e029a2db3ee557ef839b0b90b1e6802192f3
HaoDuong125 <haoduong125@gmail.com>
2017-09-24 15:12:54 +0700

    fit err not run

--- README.md
+++
@@ -1,1 +0,0 @@
-# ReactJS-Website-final-Intern
--- src/components/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const NotFoundPage = () => {
-    return (
-        <h1>Nothing to see here, move along, move along...</h1>
-    );
-};
-
-export default NotFoundPage;
